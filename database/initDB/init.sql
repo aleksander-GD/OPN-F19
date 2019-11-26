181487ff41618359ad5c37d0629f0ff72c0c0673
aleksander-GD <aldus17@student.sdu.dk>
2019-11-26 20:39:22 +0100

    database added

---
+++ database/initDB/init.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE person;
+
+USE person;
+
+CREATE TABLE personTable (
+	personID int(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+	firstname VARCHAR(100) NOT NULL,
+	lastname VARCHAR(100) NOT NULL
+);
